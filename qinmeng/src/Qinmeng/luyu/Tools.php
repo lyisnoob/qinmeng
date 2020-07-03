cadc13ac9da05264be12bb5f0537eaeea05a0234
luyu <595080590@qq.com>
2020-07-03 11:34:56 +0800

    composer 第一版

---
+++ qinmeng/src/Qinmeng/luyu/Tools.php
@@ -0,0 +1,19 @@
+<?php
+namespace Qinmeng\luyu;
+
+
+
+class Tools{
+    public function __construct()
+    {
+
+    }
+
+    public static function hello()
+    {
+        echo "hello word!";
+    }
+
+}
+
+?>
